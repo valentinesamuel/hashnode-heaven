7f9072775919148b0132a4c277708e3a4151a25f
Valentine Samuel <valentinesamuel2580@gmail.com>
2024-08-19 15:42:13 +0100

    feat: added kingston logs

--- src/index.ts
+++ src/index.ts
@@ -12,27 +12,33 @@ const app = express();
 app.use(helmet({}));
 app.use(cors());
 const defaultProxyOptions = createProxyMiddleware({
-  target: 'http://www.example.org/api', // target host with the same base path
-  changeOrigin: true, // needed for virtual hosted sites
+  target: 'http://www.example.org/api',
+  changeOrigin: true,
 });
 app.use(
   rateLimit({
-    windowMs: 1000, // 15 minutes
-    max: 2, // limit each IP to 100 requests per windowMs
+    windowMs: 1000,
+    max: 2,
   }),
 );
 app.use(express.json());
 
 app.get('/', (_: Request, res: Response) => {
-  logger.info('Hello, TypeScript is running!');
   res.send('Hello, TypeScript!');
 });
+
 app.use('/api', defaultProxyOptions);
+
 app.use('/health', (_: Request, res: Response) => {
   try {
     // Check database connection
     // const client = await pool.connect();
     // client.release();
+    logger.warn('This is a warning message', {
+      fileName: 'index.ts',
+      lineNumber: 100,
+      columnNumber: 10,
+    });
 
     res.status(200).json({
       status: 'UP',
@@ -48,15 +54,33 @@ app.use('/health', (_: Request, res: Response) => {
     });
   }
 });
+
 app.get('/error', (_req: Request, _res: Response) => {
+  logger.error(new Error('an error') as unknown as string, {
+    fileName: 'index.ts',
+    lineNumber: 100,
+    columnNumber: 10,
+  });
   throw new Error('This is a test error!');
 });
-app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
-  logger.error('Internal server error:', err.message);
-  res.status(500).send('Something went wrong!');
+
+app.get('/uncaught', (_req: Request, _res: Response, _next: NextFunction) => {
+  setTimeout(() => {
+    throw new Error('hello world');
+  }, 250);
+});
+
+app.get('/unhandled', (_req: Request, res: Response) => {
+  Promise.reject(new Error('This is an unhandled rejection!'));
+  res.send('This will not execute because of the unhandled rejection.');
+});
+
+app.use((err: Error, _req: Request, _res: Response, next: NextFunction) => {
+  logger.error('Caught exception in middleware:', err);
+  next(err);
 });
 
 const port = process.env.PORT ?? 3000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.debug('Server is running on port 3000');
 });
--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,41 +1,96 @@
-import { createLogger, format, transports } from 'winston';
+import winston, { createLogger, format, transports } from 'winston';
+import 'winston-daily-rotate-file';
+// import TelegramLogger from 'winston-telegram';
 
-// Define custom log format
-const customFormat = format.combine(
-  format.timestamp(),
-  format.errors({ stack: true }),
-  format.splat(),
-  format.json(),
-);
+const { combine, timestamp, json, errors } = format;
 
-// Create the logger
-const logger = createLogger({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  format: customFormat,
-  transports: [
-    new transports.Console({
-      format: format.combine(format.colorize(), customFormat),
-    }),
-    // new transports.File({
-    //   filename: 'logs/combined.log',
-    //   format: customFormat,
-    // }),
-    // new transports.File({
-    //   filename: 'logs/error.log',
-    //   level: 'error',
-    //   format: customFormat,
-    // }),
-  ],
+const fileRotateTransport = new winston.transports.DailyRotateFile({
+  filename: 'logs/combined-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  maxSize: '5k',
+  maxFiles: '6d',
 });
+
+fileRotateTransport.on('new', (filename) => {
+  console.log(`New log file created: ${filename}`);
+});
+fileRotateTransport.on('rotate', (oldFilename, newFilename) => {
+  console.log(`Log file rotated: ${oldFilename} -> ${newFilename}`);
+});
+fileRotateTransport.on('archive', (zipFilename) => {
+  console.log(`Log files archived: ${zipFilename}`);
+});
+fileRotateTransport.on('logRemoved', (removedFilename) => {
+  console.log(`Log file removed: ${removedFilename}`);
+});
+
+const logger = createLogger({
+  levels: winston.config.npm.levels,
+  level: process.env.LOG_LEVEL ?? 'info',
+  format: combine(
+    errors({ stack: true }),
+    timestamp({ format: 'YYYY-MM-DD hh:mm:ss.SSS A' }),
+    json(),
+  ),
+  transports: [new transports.Console(), fileRotateTransport],
+  exceptionHandlers: [new transports.File({ filename: 'logs/exceptions.log' })],
+  rejectionHandlers: [new transports.File({ filename: 'logs/rejections.log' })],
+});
+
+// const errorFilter = winston.format((info, opts) => {
+//   return info.level === 'error' ? info : false;
+// });
+
+// const infoFilter = winston.format((info, opts) => {
+//   return info.level === 'info' ? info : false;
+// });
+
+// // Create the logger
+// const logger = createLogger({
+//   levels: winston.config.syslog.levels,
+//   level: process.env.LOG_LEVEL || 'info',
+//   format: combine(
+//     errors({ stack: true }),
+//   timestamp({
+//     format: 'YYYY-MM-DD hh:mm:ss.SSS A',
+//   }),
+//   align(),
+//   printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`),
+// ),
+// transports: [
+//   new transports.Console(),
+//   new transports.File({
+//     filename: 'logs/combined.log',
+//   }),
+//   new transports.File({
+//     filename: 'logs/error.log',
+//     level: 'error',
+//     format: combine(errorFilter(), timestamp(), json()),
+//   }),
+//   new transports.File({
+//     filename: 'logs/info.log',
+//     level: 'info',
+//     format: combine(infoFilter(), timestamp(), json()),
+//   }),
+// ],
+// exceptionHandlers: [
+//   new transports.File({ filename: 'logs/exception.log' }),
+// ],
+// rejectionHandlers: [
+//   new transports.File({ filename: 'logs/rejections.log' }),
+// ],
+// });
 
 // Handle unhandled rejections
 process.on('unhandledRejection', (error) => {
   logger.error('Unhandled Rejection at:', error);
+  process.exitCode = 1;
 });
 
 // Handle uncaught exceptions
 process.on('uncaughtException', (error) => {
-  logger.error('Uncaught Exception thrown:', error);
+  logger.error('Uncaught Exception thrown=>>>>:', error);
+  process.exitCode = 1;
 });
 
 export default logger;
