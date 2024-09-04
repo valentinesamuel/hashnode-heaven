9135d7a4337c5e8b21e4f77ad8174385b2a226d1
Valentine Samuel <valentinesamuel2580@gmail.com>
2024-09-04 08:45:54 +0100

    feat: added connection pooling

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -10,5 +10,11 @@ export const PgDataSource = new DataSource({
   entities: [__dirname + '/entity/**/*.ts'],
   migrations: [__dirname + '/migration/**/*.ts'],
   synchronize: true,
-  logging: true
+  logging: true,
+  extra: {
+    max: 20, // Maximum number of clients in the pool
+    idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
+    connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
+    
+  },
 });
